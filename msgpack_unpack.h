8692821bc3eb325920eedda0496feacde66b68c9
Xinchen Hui <laruence@gmail.com>
2013-02-20 21:26:59 +0800

    Fix Bug #16 (implicit declaration of 2 functions)

--- msgpack_unpack.h
+++ msgpack_unpack.h
@@ -26,6 +26,8 @@ typedef struct {
     int type;
 } msgpack_unserialize_data;
 
+void msgpack_serialize_var_init(msgpack_serialize_data_t *var_hash);
+void msgpack_serialize_var_destroy(msgpack_serialize_data_t *var_hash);
 void msgpack_unserialize_var_init(msgpack_unserialize_data_t *var_hashx);
 void msgpack_unserialize_var_destroy(
     msgpack_unserialize_data_t *var_hashx, zend_bool err);
