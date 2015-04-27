14f05a79620f896c38571ace483b58b2bbcbed85
Xinchen Hui <laruence@gmail.com>
2015-04-27 11:35:18 +0800

    back to dev

--- php_msgpack.h
+++ php_msgpack.h
@@ -3,7 +3,7 @@
 
 #include "ext/standard/php_smart_str.h" /* for smart_str */
 
-#define PHP_MSGPACK_VERSION "0.5.6"
+#define PHP_MSGPACK_VERSION "0.5.7-dev"
 
 extern zend_module_entry msgpack_module_entry;
 #define phpext_msgpack_ptr &msgpack_module_entry
