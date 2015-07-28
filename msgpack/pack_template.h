msgpack/msgpack-php

commit 66f1e04fcfe34f4cb6b9e20b4815bb79eb830f43

GH Issue #49, implement the 'compatibility mode' mentioned in msgpack implementation guidelines. When msgpack.use_str8_serialization is set to 0 str serialization will never serialize a string using str8

msgpack/pack_template.h
@@ -730,7 +730,7 @@ msgpack_pack_inline_func(_raw)(msgpack_pack_user x, size_t l)
 	if(l < 32) {
 		unsigned char d = 0xa0 | l;
 		msgpack_pack_append_buffer(x, &TAKE8_8(d), 1);
-	} else if (l < 256) {
+	} else if (l < 256 && MSGPACK_G(use_str8_serialization)) {
 		unsigned char buf[2];
 		buf[0] = 0xd9; buf[1] = (uint8_t)l;
 		msgpack_pack_append_buffer(x, buf, 2);

msgpack.c
@@ -45,6 +45,9 @@ STD_PHP_INI_BOOLEAN(
 STD_PHP_INI_BOOLEAN(
     "msgpack.illegal_key_insert", "0", PHP_INI_ALL, OnUpdateBool,
     illegal_key_insert, zend_msgpack_globals, msgpack_globals)
+STD_PHP_INI_BOOLEAN(
+    "msgpack.use_str8_serialization", "1", PHP_INI_ALL, OnUpdateBool,
+    use_str8_serialization, zend_msgpack_globals, msgpack_globals)
 PHP_INI_END()
 
 #if HAVE_PHP_SESSION
@@ -77,6 +80,7 @@ static void msgpack_init_globals(zend_msgpack_globals *msgpack_globals)
     msgpack_globals->illegal_key_insert = 0;
     msgpack_globals->serialize.var_hash = NULL;
     msgpack_globals->serialize.level = 0;
+    msgpack_globals->use_str8_serialization = 1;
 }
 
 static ZEND_MINIT_FUNCTION(msgpack)

php_msgpack.h
@@ -24,6 +24,7 @@ ZEND_BEGIN_MODULE_GLOBALS(msgpack)
     zend_bool error_display;
     zend_bool php_only;
     zend_bool illegal_key_insert;
+    zend_bool use_str8_serialization;
     struct {
         void *var_hash;
         unsigned level;

tests/029.phpt
@@ -8,7 +8,7 @@ if (!extension_loaded("session")) {
 }
 ?>
 --FILE--
-<?php 
+<?php
 ob_start();
 phpinfo(INFO_MODULES);
 $str = ob_get_clean();
@@ -51,3 +51,4 @@ Directive => Local Value => Master Value
 msgpack.error_display => On => On
 msgpack.illegal_key_insert => Off => Off
 msgpack.php_only => On => On
+msgpack.use_str8_serialization => On => On

tests/137.phpt
@@ -7,21 +7,28 @@ unpack/pack str8
 	}
 --FILE--
 <?php
-	function test() {
-		if(!extension_loaded('msgpack'))
-		{
-			dl('msgpack.' . PHP_SHLIB_SUFFIX);
-		}
-
-		$str = "Simple test for short string - type str8";
-		$str8 = chr(0xD9) . chr(strlen($str)) . $str;
-		echo msgpack_unpack($str8) . "\n";
-		$data = msgpack_pack($str);
-		echo ($data[0] == chr(0xD9) && $data[1] == chr(strlen($str)) ?  "OK" : "FAILED"), PHP_EOL;
+	if(!extension_loaded('msgpack')) {
+		dl('msgpack.' . PHP_SHLIB_SUFFIX);
 	}
 
-	test();
+	$str = "Simple test for short string - type str8";
+	$str8 = chr(0xD9) . chr(strlen($str)) . $str;
+	echo msgpack_unpack($str8) . "\n";
+
+	//assert str8 serialization works, and default for use use_str8_serialization is 1
+	$data = msgpack_pack($str);
+	var_dump(bin2hex($data));
+	echo ($data[0] == chr(0xD9) && $data[1] == chr(strlen($str)) ?  "OK" : "FAILED"), PHP_EOL;
+
+	ini_set('msgpack.use_str8_serialization', 0);
+	$data = msgpack_pack($str);
+	var_dump(bin2hex($data));
+	echo ($data[0] == chr(0xDA) && $data[2] == chr(strlen($str)) ?  "OK" : "FAILED"), PHP_EOL;
+
 ?>
 --EXPECTF--
 Simple test for short string - type str8
+string(84) "d92853696d706c65207465737420666f722073686f727420737472696e67202d20747970652073747238"
+OK
+string(86) "da002853696d706c65207465737420666f722073686f727420737472696e67202d20747970652073747238"
 OK

commit 0b80d304359a2b7c633436ef40b7fd4de1c29aac

66f1e04fcfe34f4cb6b9e20b4815bb79eb830f43 broke ZTS builds, add TSRMLS_FETCH call before querying use_str8_serialization when packing a str

msgpack/pack_template.h
@@ -727,6 +727,7 @@ msgpack_pack_inline_func(_map)(msgpack_pack_user x, unsigned int n)
 
 msgpack_pack_inline_func(_raw)(msgpack_pack_user x, size_t l)
 {
+	TSRMLS_FETCH();
 	if(l < 32) {
 		unsigned char d = 0xa0 | l;
 		msgpack_pack_append_buffer(x, &TAKE8_8(d), 1);