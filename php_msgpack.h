b285146a1cb2641c5a07167131bdd0a8ac51cf0d
Michael Wallner <mike@php.net>
2021-08-23 20:20:37 +0200

    PHP-7.4 compatibility

--- php_msgpack.h
+++ php_msgpack.h
@@ -58,4 +58,43 @@ PHP_MSGPACK_API int php_msgpack_unserialize(
 # define OBJ_FOR_PROP(zv) Z_OBJ_P(zv)
 #endif
 
+#if PHP_VERSION_ID >= 80000
+# define IS_MAGIC_SERIALIZABLE(ce) (ce && ce->__serialize)
+# define CALL_MAGIC_SERIALIZE(cep, zop, rvp) zend_call_known_instance_method_with_0_params((cep)->__serialize, zop, rvp)
+# define CALL_MAGIC_UNSERIALIZE(cep, zop, rvp, zsdap) zend_call_known_instance_method_with_1_params((cep)->__unserialize, zop, rvp, zsdap)
+#elif PHP_VERSION_ID >= 70400
+# define IS_MAGIC_SERIALIZABLE(ce) (ce && zend_hash_str_exists(&ce->function_table, ZEND_STRL("__serialize")))
+# define CALL_MAGIC_SERIALIZE(cep, zop, rvp) call_magic_serialize_fn(cep, zop, rvp, ZEND_STRL("__serialize"), 0, NULL)
+# define CALL_MAGIC_UNSERIALIZE(cep, zop, rvp, zsdap) call_magic_serialize_fn(cep, zop, rvp, ZEND_STRL("__unserialize"), 1, zsdap)
+static inline void call_magic_serialize_fn(zend_class_entry *ce, zend_object *object, zval *retval_ptr, const char *fn_str, size_t fn_len, int param_count, zval *params) {
+	zval retval;
+	zend_fcall_info fci;
+	zend_fcall_info_cache fcic;
+
+	fci.size = sizeof(fci);
+	fci.object = object;
+	fci.retval = retval_ptr ? retval_ptr : &retval;
+	fci.param_count = param_count;
+	fci.params = params;
+	ZVAL_UNDEF(&fci.function_name); /* Unused */
+
+	fcic.function_handler = zend_hash_str_find_ptr(&ce->function_table, fn_str, fn_len);
+	fcic.object = object;
+	fcic.called_scope = ce;
+
+	int result = zend_call_function(&fci, &fcic);
+	if (UNEXPECTED(result == FAILURE)) {
+		if (!EG(exception)) {
+			zend_error_noreturn(E_CORE_ERROR, "Couldn't execute method %s%s%s",
+				fcic.function_handler->common.scope ? ZSTR_VAL(fcic.function_handler->common.scope->name) : "",
+				fcic.function_handler->common.scope ? "::" : "", ZSTR_VAL(fcic.function_handler->common.function_name));
+		}
+	}
+
+	if (!retval_ptr) {
+		zval_ptr_dtor(&retval);
+    }
+}
+#endif
+
 #endif  /* PHP_MSGPACK_H */
